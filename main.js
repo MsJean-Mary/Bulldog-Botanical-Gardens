073b9997ad323b06faf8b9a6723232a90067ffd3
Xordas <120686481+Xordas@users.noreply.github.com>
2024-12-10 15:27:22 +0000

    contact page

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
