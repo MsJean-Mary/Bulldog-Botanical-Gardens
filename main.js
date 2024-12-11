f2a6e7e96c1d7671b92d9673ef9f464ffb7d31eb
TobyTheIdiot <tmhancock2@students.wcpss.net>
2024-12-11 15:33:13 +0000

    start blog page

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog.html'));
+});
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
